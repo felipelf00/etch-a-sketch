95feb9d0b6cca5b6cf818c3bd4366fdc55d69f11
felipewantstocode <118938786+felipewantstocode@users.noreply.github.com>
2023-01-20 21:05:34 -0300

    Create basic logic for painting

---
+++ script.js
@@ -0,0 +1,25 @@
+
+const container = document.querySelector(".container");
+
+
+function createGrid(side, container) {
+    for (let i = 0; i < side**2; i++ ) {
+        let div = document.createElement('div');
+        div.classList.add("pixel");
+        container.appendChild(div);
+    }
+
+}
+
+let side = 16;
+createGrid (side, container);
+
+const pixels = document.querySelectorAll(".pixel");
+
+pixels.forEach( (pixel) => {
+    pixel.addEventListener("mouseenter", (e) => {
+        if (e.which === 1) {
+           pixel.classList.toggle("painted");
+        }
+    });
+});
