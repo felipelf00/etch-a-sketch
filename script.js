608abd49e92fa0e023b68da20b4c9d3d3f4258a5
felipewantstocode <118938786+felipewantstocode@users.noreply.github.com>
2023-01-21 16:06:23 -0300

    Added grid size personalization and organized functions

--- script.js
+++ script.js
@@ -1,25 +1,47 @@
 
-const container = document.querySelector(".container");
-
-
 function createGrid(side, container) {
+    let size = (container.offsetWidth - 2) / side;
     for (let i = 0; i < side**2; i++ ) {
         let div = document.createElement('div');
         div.classList.add("pixel");
+        div.style.width = size+'px';
+        div.style.height = size+'px';
         container.appendChild(div);
     }
 
 }
 
-let side = 16;
-createGrid (side, container);
-
-const pixels = document.querySelectorAll(".pixel");
-
-pixels.forEach( (pixel) => {
-    pixel.addEventListener("mouseenter", (e) => {
-        if (e.which === 1 && !pixel.classList.contains("painted")) {
-           pixel.classList.toggle("painted");
-        }
+function enablePaint () {
+    const pixels = document.querySelectorAll(".pixel");
+    //e.which returns which mouse button is pressed.
+    pixels.forEach( (pixel) => {
+        pixel.addEventListener("mouseenter", (e) => {
+            if (e.which === 1 && !pixel.classList.contains("painted")) { 
+            pixel.classList.toggle("painted");
+            }
+        });
     });
-});
+}
+
+function clearGrid () {
+    const container = document.querySelector(".container");
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+}
+
+const container = document.querySelector(".container");
+let side = 32;
+createGrid (side, container);
+enablePaint();
+
+const btn = document.querySelector("#button");
+btn.addEventListener('click', (side) => {
+    side = document.getElementById("input").value;
+    const container = document.querySelector(".container");
+    clearGrid();
+    createGrid(side, container);
+    enablePaint();
+})
+
+
